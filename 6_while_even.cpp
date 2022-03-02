87050176ad12700c1d570d92739a4ce98c9c05e2
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2022-03-02 15:03:19 +0530

    Add files via upload

---
+++ 6_while_even.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,i;
+    cin>>n;
+    i=2;
+    while (i<=n)
+    {   
+        cout<<i<<' ';
+        i=i+2;
+    }
+    return 0;
+}
